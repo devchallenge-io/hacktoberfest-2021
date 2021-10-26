6c7a122cd06b8cc0f2eb4a78771122b8d45f3e37
Filipe Batista <filipe.x@live.com>
2021-10-26 14:17:15 -0300

    desafio 4 entregue

---
+++ resolucoes/desafio4/react-filipeleonelbatista/src/routes.js
@@ -0,0 +1,14 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+
+function Routes() {
+  return (
+    <BrowserRouter >
+    <Switch>
+        <Route path="/" exact component={Home} />
+    </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
